1b3f1bf0137b048a0aa263440fd201cc562cd407
doodletaco <47832687+doodletaco@users.noreply.github.com>
2021-03-29 00:49:06 -0700

    Day 2. A day of rough polishing,
    Made README.txt with some information.
    Added music and some more sound effects.
    Polished the menu and added scorekeeping and a lives system.
    Fixed a screen error.

---
+++ README.txt
@@ -0,0 +1,26 @@
+DEPENDENCIES:
+This game requires the pygame and random modules in order to run.
+
+HOW TO RUN:
+Open the file "run_game.py"
+A python interpreter is required.
+
+CONTROLS:
+This game is played entirely with the arrow keys.
+
+In the menu
+- Up arrow: Begin the game
+In game
+- Arrow keys: Press the one in the direction of the red square.
+
+GAMEPLAY:
+Use the arrow keys to steal the corresponding square.
+You can make a mistake 3 times.
+The game records your high score, but it will not save after being closed.
+
+COPYRIGHT ATTRIBUTIONS:
+Music created by SketchyLogic with CC-BY 3.0 License
+ - License info: https://creativecommons.org/licenses/by/3.0/
+ - Music sourced from: https://opengameart.org/content/hungry-dino-9-chiptune-tracks-10-sfx
+SFX created in BFXR
+ - Link to BFXR: https://www.bfxr.net/
---
+++ run_game.py
@@ -0,0 +1,220 @@
+# import and initialize pygame
+import pygame, random
+pygame.init()
+
+# set up the screen
+dis_w = 600
+dis_h = 600
+dis_size = (dis_w, dis_h)
+
+dis = pygame.display.set_mode(dis_size)
+pygame.display.set_caption('Pyweek 31')
+
+# create a color
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+# invent time
+clock = pygame.time.Clock()
+
+# create the images used
+center_image = pygame.image.load('images/placeholder_center.png')
+enemy_image = pygame.image.load('images/placeholder_enemy.png')
+enemy_image_needsclick = pygame.image.load('images/placeholder_enemy_needsclick.png')
+screen_image = pygame.image.load('images/screen.png')
+
+# load the audio files
+blip = pygame.mixer.Sound('audio/Blip_Select.wav')
+hurt = pygame.mixer.Sound('audio/Hit_Hurt2.wav')
+lost_life_sound = pygame.mixer.Sound('audio/Hit_Hurt4.wav')
+start_sound = pygame.mixer.Sound('audio/Jump.wav')
+
+menu_song = pygame.mixer.music.load('audio/Calm.wav')
+game_song = pygame.mixer.music.load('audio/FranticLevel.wav')
+
+# load fonts
+font = pygame.font.SysFont('arial', 25)
+
+#some score variables
+score = 0
+high_score = 0
+lives = 3
+
+# make angy square bois
+class Enemy:
+    # initialize the class
+    def __init__(self, rct, key):
+        self.rectangle = rct
+        self.key = key
+        self.image = enemy_image
+        self.needsClick = False
+
+    # change the image to the square that needs to be clicked
+    def get_angy(self):
+        if not self.needsClick:
+            self.needsClick = True
+            self.image = enemy_image_needsclick
+
+    # if the square needed to be clicked,
+    def calm_down(self):
+        global score, high_score, lives
+        # checks if the corresponding square needs to be clicked
+        if self.needsClick:
+            self.needsClick = False
+            self.image = enemy_image
+            score += 1
+            if score > high_score:
+                high_score = score
+
+            blip.play()
+        # if not, end the game
+        else:
+            if lives > 1:
+                lost_life_sound.play()
+                lives -= 1
+            else:
+                score = 0
+                lives = 3
+                pygame.mixer.music.stop()
+                hurt.play()
+                end_screen()
+
+
+# draw the window and all the stuff in it
+def draw_window(center, e1, e2, e3, e4, score):
+    global lives
+    dis.fill(black)
+
+    dis.blit(center_image, (center.x, center.y))
+
+    dis.blit(e1.image, (e1.rectangle.x, e1.rectangle.y))
+    dis.blit(e2.image, (e2.rectangle.x, e2.rectangle.y))
+    dis.blit(e3.image, (e3.rectangle.x, e3.rectangle.y))
+    dis.blit(e4.image, (e4.rectangle.x, e4.rectangle.y))
+
+    score_text = 'Hearts Copped: ' + str(score)
+    score_surface = font.render(score_text, False, white)
+    dis.blit(score_surface, (15, 15))
+
+    lives_counter = 'Lives: '
+    for i in range(lives):
+        lives_counter += 'X '
+    lives_surface = font.render(lives_counter, False, white)
+    dis.blit(lives_surface, (15, 45))
+
+    pygame.display.update()
+
+
+# game over screen
+def end_screen():
+    global high_score, play_time
+    pygame.mixer.music.stop()
+    pygame.mixer.music.load('audio/FranticLevel.wav')
+    pygame.mixer.music.play(-1)
+    run = True
+    while run:
+        # track events
+        for event in pygame.event.get():
+            # detect when exit button pressed
+            if event.type == pygame.QUIT:
+                run = False
+            # detect key presses
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    main()
+
+        dis.blit(screen_image, (0, 0))
+        # display text and stuff
+        words = 'Most Hearts Copped: ' + str(high_score)
+        textsurface = font.render(words, False, white)
+
+        more_words = 'Press the up arrow to play.'
+        other_textsurface = font.render(more_words, False, white)
+
+        dis.blit(textsurface, (15, 15))
+        dis.blit(other_textsurface, (15, 40))
+
+        pygame.display.update()
+
+    pygame.quit()
+    quit()
+
+
+# causes a random square to be activated if there are no others
+def anger(enemies):
+    none_angy = True
+    for e in enemies:
+        if e.needsClick:
+            last_angered = e
+            none_angy = False
+    if none_angy:
+        choice = random.choice(enemies).get_angy()
+
+
+# main loop
+def main():
+    global score, high_score
+    # create rectangles
+    center = pygame.Rect(250, 250, 100, 100)
+
+    enemy1rect = pygame.Rect(287, 100, 26, 26)
+    enemy2rect = pygame.Rect(287, 474, 26, 26)
+    enemy3rect = pygame.Rect(100, 278, 26, 26)
+    enemy4rect = pygame.Rect(474, 275, 26, 26)
+
+    #initialize enemy classes
+    e1 = Enemy(enemy1rect, 'up')
+    e2 = Enemy(enemy2rect, 'down')
+    e3 = Enemy(enemy3rect, 'left')
+    e4 = Enemy(enemy4rect, 'right')
+
+    enemies = [e1, e2, e3, e4]
+
+    pygame.mixer.music.stop()
+    pygame.mixer.music.load('audio/Calm.wav')
+    pygame.mixer.music.play(-1)
+
+    start_sound.play()
+
+    # loop
+    run = True
+    while run:
+        key_pressed = ''
+        # track events
+        for event in pygame.event.get():
+            # detect when exit button pressed
+            if event.type == pygame.QUIT:
+                run = False
+            # detect key presses
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    key_pressed = 'up'
+                if event.key == pygame.K_DOWN:
+                    key_pressed = 'down'
+                if event.key == pygame.K_LEFT:
+                    key_pressed = 'left'
+                if event.key == pygame.K_RIGHT:
+                    key_pressed = 'right'
+
+        # trigger calm down for each corresponding square
+        if key_pressed != '':
+            for e in enemies:
+                if key_pressed == e.key:
+                    e.calm_down()
+
+        # activate a square if there needs to be one
+        anger(enemies)
+
+        # draw the window
+        draw_window(center, e1, e2, e3, e4, score)
+        # cap framerate
+        clock.tick(60)
+
+    # quit the game
+    pygame.quit()
+    quit()
+
+
+# make sure correct one starts
+if __name__ == "__main__":
+    end_screen()
